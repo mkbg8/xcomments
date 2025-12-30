eec80b8fe4fa57702d72d8f805de54780692b984
Michael <mike@braing8.com>
2025-12-30 07:49:43 +0200

    Initial commit

---
+++ src/test/samples/javascript.js
@@ -0,0 +1,25 @@
+
+/*
+? My Class
+! Creating some Copyright Notes
+*/
+
+/*
+  & My Function
+  ! Some Alert
+  ^ Some stuff
+  ? Questions
+  * Highlights
+  ~ Arrow Function
+  TODO Colorful 
+*/
+
+var myFunction = function(myParam) {
+  return myParam;
+  // ^ Tested all Cases
+}
+
+//// removed code
+
+// * End of the file
+
---
+++ src/test/samples/markdown.md
@@ -0,0 +1,16 @@
+### Some Markdown
+## Some other header
+
+* bullet point
+* bullet point 2
+
+<!-- Comments -->
+<!-- ! Comments -->
+<!-- * Comments -->
+<!-- ? Comments -->
+<!-- ^ Comments -->
+<!--
+  TODO Comments
+  ~ Comments
+  & Comments 
+-->
